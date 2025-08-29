6a54f6d20638dffb4e3d56c6ea8029672da08cba
y22jun <tlsdpwns0612@naver.com>
2025-08-29 17:19:00 +0900

    feat: add BaseTimeEntity and custom exception setup

---
+++ src/main/java/team/bridgers/backend/BridgersApplication.java
@@ -0,0 +1,13 @@
+package team.bridgers.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridgersApplication.class, args);
+    }
+
+}
---
+++ src/main/java/team/bridgers/backend/global/exception/ExceptionCode.java
@@ -0,0 +1,13 @@
+package team.bridgers.backend.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionCode {
+
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+
+}
