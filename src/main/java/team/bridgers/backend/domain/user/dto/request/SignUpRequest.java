d483a02e3530a95820f639b6c02ffde9a6e0ca2d
y22jun <tlsdpwns0612@naver.com>
2025-09-03 19:27:12 +0900

    refactor: 회원가입 검증 수정

--- src/main/java/team/bridgers/backend/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/team/bridgers/backend/domain/user/dto/request/SignUpRequest.java
@@ -1,5 +1,6 @@
 package team.bridgers.backend.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
@@ -16,28 +17,27 @@ public record SignUpRequest (
         String email,
 
         @NotBlank
-//        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d){1,12}$")
+        @Email
         String loginId,
 
         @NotBlank
-//        @Pattern(regexp = "^")
         String nickname,
 
         @NotBlank
-//        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$")
         String password,
 
         @NotBlank
-//        @Pattern(
-//                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
-//                message = "<8~15자의 영문 대/소문자, 숫자, 특수문자 포함> 비밀번호 조건을 충족해야 합니다."
-//        )
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+                message = "<8~15자의 영문 대/소문자, 숫자, 특수문자 포함> 비밀번호 조건을 충족해야 합니다."
+        )
         String confirmPassword,
 
         Gender gender,
 
         @NotBlank
-//        @Pattern(regexp = "^(?=.*\\d).{8}$")
+        @Pattern(regexp = "^(?=.*\\d).{8}$")
         String birthday,
 
         UserType type
